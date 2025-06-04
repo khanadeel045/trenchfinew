3f20dc8c70c44f4cc75a755ab851c3d08af7014f
Muhammad Adeel Bin Saeed <59364803+khanadeel045@users.noreply.github.com>
2025-06-04 15:47:42 +0500

    fix: improve mongodb connection

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,23 +1,23 @@
 // /src/lib/mongodb.js
 import mongoose from 'mongoose';
 
-let cached = global.mongoose;
+// Use `globalThis` so the cache works across serverless function invocations
+let cached = globalThis.mongoose;
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+  cached = globalThis.mongoose = { conn: null, promise: null };
 }
 
 async function connectToDatabase() {
   if (cached.conn) {
     return cached.conn;
   }
+
   if (!cached.promise) {
     cached.promise = mongoose
-      .connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+      .connect(process.env.MONGODB_URI)
       .then((mongoose) => mongoose);
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
