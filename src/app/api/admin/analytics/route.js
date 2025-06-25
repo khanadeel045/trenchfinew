343463581cb760846cfece5d01b4bc5325613c26
Muhammad Adeel Bin Saeed <59364803+khanadeel045@users.noreply.github.com>
2025-06-25 22:47:06 +0500

    Delete route.js

--- src/app/api/admin/analytics/route.js
+++
@@ -1,8 +0,0 @@
-// /app/api/admin/analytics/route.js
-import { NextResponse } from 'next/server';
-
-export async function GET(request) {
-  const userData = await getUserStats();
-  const revenueData = await getRevenueStats();
-  return NextResponse.json({ userData, revenueData });
-}
