788101d20b50dfc787a050f49eca100e2c17a754
Muhammad Adeel Bin Saeed <59364803+khanadeel045@users.noreply.github.com>
2025-07-02 18:43:43 +0500

    Update page.js

--- src/app/account/history/page.js
+++ src/app/account/history/page.js
@@ -33,7 +33,7 @@ export default function HistoryPage() {
         ) : (
           <div className="grid gap-4">
             {liked.map((video) => (
-              <VideoCard key={video._id} video={video} currentUser={me} />
+              // <VideoCard key={video._id} video={video} currentUser={me} />
             ))}
           </div>
         )}
@@ -46,7 +46,7 @@ export default function HistoryPage() {
         ) : (
           <div className="grid gap-4">
             {commented.map((video) => (
-              <VideoCard key={video._id} video={video} currentUser={me} />
+              // <VideoCard key={video._id} video={video} currentUser={me} />
             ))}
           </div>
         )}
